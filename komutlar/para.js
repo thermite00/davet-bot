ec6d6e2b18e7cd1742c0d44e6a712e179562a148
Glitch (energetic-sky) <none>
2019-11-15 19:10:57 +0000

    💶🍭 Checkpoint ./komutlar/para.js:3324753/3901

--- komutlar/para.js
+++ komutlar/para.js
@@ -1,33 +1,38 @@
-const Discord = require("discord.js")
-const db = require("quick.db")
+const Discord = require("discord.js");
+const db = require("quick.db");
 
-exports.run = async(msg, message, args) => {
-    let para = await db.fetch(`para_${message.guild.id}_${message.author.id}`);
-  if(para == null){
-    const embed = new Discord.RichEmbed()
-  .addField("Paran:", `0₺`)
-  .setColor("BLACK")
-message.channel.send(embed)
-    return
-  } else{
-  const embed = new Discord.RichEmbed()
-  .addField("Paran:", para+`₺`)
-  .setColor("BLACK")
-message.channel.send(embed)
+exports.run = async (client, message, args) => {
+  let para = await db.fetch(`para_${message.guild.id}_${client.user.id}`);
+  let user;
+  if (message.mentions.users.first()) {
+    user = message.mentions.users.first();
+  }else {
+    user = message.author;
   }
-}
-
+  if (para == null) {
+    const embed = new Discord.RichEmbed()
+      .addField("Paran:", `0₺`)
+      .setColor("BLACK");
+    message.channel.send(embed);
+    return;
+  } else {
+    const embed = new Discord.RichEmbed()
+      .addField("Paran:", para + `₺`)
+      .setColor("BLACK");
+    message.channel.send(embed);
+  }
+};
 
 exports.conf = {
-  enabled: true, 
-  guildOnly: false, 
-  aliases: ['pg'],
+  enabled: true,
+  guildOnly: false,
+  aliases: ["pg"],
   permLevel: 0,
-  kategori: 'puan'
+  kategori: "puan"
 };
 
 exports.help = {
-  name: 'para', 
-  description: 'Puanınızı gösterir.',
-  usage: 'para'
+  name: "para",
+  description: "Puanınızı gösterir.",
+  usage: "para"
 };
