679423bad134acd84e09188ee09382e030d57c23
Glitch (energetic-sky) <none>
2019-11-16 08:09:57 +0000

    😘⛰ Checkpoint ./komutlar/rulet.js:3324753/3608

--- komutlar/rulet.js
+++ komutlar/rulet.js
@@ -1,14 +1,22 @@
 const Discord = require("discord.js");
 const db = require("quick.db");
 const { stripIndents } = require("common-tags");
-const slots = ["Siyah 1", "Kırmızı 2", "Siyah 3", "Kırmızı 4", "Siyah 5", "Kırmızı 6", "Siyah 7", "Kırmızı 8", "Siyah 9", "Kırmızı 10", "Siyah 11", "Kırmızı 12", "Siyah 13", "Kırmızı 14", "Siyah 15", "Kırmızı 16", "Siyah 17", "Kırmızı 18", "Siyah 19", "Kırmızı 20", "Siyah 21", "Kırmızı 22", "Siyah 23", "Kırmızı 24", "Siyah 25", "Kırmızı 26", "Siyah 27", "Kırmızı 28", "Siyah "];
+const slots = ["Siyah 1", "Kırmızı 2", "Siyah 3", "Kırmızı 4", "Siyah 5", "Kırmızı 6", "Siyah 7", "Kırmızı 8", "Siyah 9", "Kırmızı 10", "Siyah 11", "Kırmızı 12", "Siyah 13", "Kırmızı 14", "Siyah 15", "Kırmızı 16", "Siyah 17", "Kırmızı 18", "Siyah 19", "Kırmızı 20", "Siyah 21", "Kırmızı 22", "Siyah 23", "Kırmızı 24", "Siyah 25", "Kırmızı 26", "Siyah 27", "Kırmızı 28", "Siyah 29", "Kırmızı 30", "Siyah 31", "Kırmızı 32", "Siyah 33", "Kırmızı 34", "Siyah 35", "Kırmızı 36", "Kırmızı", "Siyah"];
 
 exports.run = async (client, message, args) => {
   let para = await db.fetch(`para_${message.guild.id}_${message.author.id}`);
   let sembol = (await db.fetch(`psembol_${message.guild.id}`)) || "₺";
-  let miktar = args[0];
+  let miktar = args[1];
+  let birim = args.slice(0).join(" ")
   var slot1 = slots[Math.floor(Math.random() * slots.length)];
-
+if(!birim == "Siyah 1" || "Kırmızı 2" || "Siyah 3" || "Kırmızı 4" || "Siyah 5" || "Kırmızı 6" || "Siyah 7" || "Kırmızı 8" || "Siyah 9" || "Kırmızı 10" || "Siyah 11" || "Kırmızı 12" || "Siyah 13" || "Kırmızı 14" || "Siyah 15" || "Kırmızı 16" || "Siyah 17" || "Kırmızı 18" || "Siyah 19" || "Kırmızı 20" || "Siyah 21" || "Kırmızı 22" || "Siyah 23" || "Kırmızı 24" || "Siyah 25" || "Kırmızı 26" || "Siyah 27" || "Kırmızı 28" || "Siyah 29" || "Kırmızı 30" || "Siyah 31" || "Kırmızı 32" || "Siyah 33" || "Kırmızı 34" || "Siyah 35" || "Kırmızı 36" || "Kırmızı" || "Siyah"){
+  const embed = new Discord.RichEmbed()
+      .setTitle("Hata!")
+      .setColor("BLACK")
+      .setDescription(`Hatalı birim!\nGirelecek değerler;\nSiyah 1, Kırmızı 2, Siyah 3, Kırmızı 4, Siyah 5, Kırmızı 6, Siyah 7, Kırmızı 8 gibidir! siyah ile başlar kırmızı ile biter! 1-36 arasıda bir seçim yapınız.\nÇift sayılar kırmızıya düşer, tek sayılar siyaha.`);
+    message.channel.send(embed);
+    return;
+}
   if (!miktar) {
     const embed = new Discord.RichEmbed()
       .setTitle("Hata!")
@@ -32,30 +40,14 @@ exports.run = async (client, message, args) => {
     message.channel.send(embed);
     return;
   }
-  if (slot1 === slot2 && slot1 === slot3) {
+  if (slot1 === birim) {
     const embed = new Discord.RichEmbed().setColor("BLACK")
-      .setDescription(stripIndents`
-   ${slot6} : ${slot4} : ${slot3} 
-
-	 ${slot1} : ${slot2} : ${slot3}  <==
-
-   ${slot1} : ${slot4} : ${slot5}  
-
-		Tebrikler, kazandınız!
-		`);
-    db.add(`para_${message.guild.id}_${message.author.id}`, +miktar, +miktar);
+      .setDescription(`Tebrikler Kazandınız!\nSonuç;\nSeçtiğiniz: ${birim}\nÇıkan: ${slot1}`);
+    db.add(`para_${message.guild.id}_${message.author.id}`, +miktar, +miktar, +miktar);
     message.channel.send(embed);
   } else {
     const embed = new Discord.RichEmbed().setColor("BLACK")
-      .setDescription(stripIndents`
-   ${slot6} : ${slot4} : ${slot3} 
-
-	 ${slot1} : ${slot2} : ${slot3} <===
-
-   ${slot1} : ${slot4} : ${slot5} 
-
-		Kaybettiniz!
-		`);
+      .setDescription(`Kaybettiniz!\nSonuç;\nSeçtiğiniz: ${birim}\nÇıkan: ${slot1}`);
     db.add(`para_${message.guild.id}_${message.author.id}`, -miktar);
     message.channel.send(embed);
   }
