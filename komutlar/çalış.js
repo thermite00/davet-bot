1ad00c26e4d873ce7f14b6e1d67217ce58d92958
Glitch (energetic-sky) <none>
2019-11-17 08:48:09 +0000

    🕰🔋 Checkpoint ./komutlar/çalış.js:3324753/450 ./komutlar/çek.js:3324753/138

--- komutlar/çalış.js
+++ komutlar/çalış.js
@@ -3,8 +3,10 @@ const Discord = require("discord.js"),
   ms = require("parse-ms");
 
 exports.run = async (bot, message, args) => {
+  const cevaplar = ("Yaşlıları karşıdan karşıya geçirdin. İyiliğinden dolayı ödüllendirildin!", "Barınak'da ki köpekleri gezmeye çıkardın, Barınak sahibi sana para verdi!", "Sokakta ki çocuklara balon sattın", "Youtuber oldun ve videolardan para kazanmaya başladın!", "Sokakta dilencilik yaparak para kazandın")
   let cooldown = await db.fetch(`çalışs_${message.guild.id}`) || "300000";
   let amount = Math.floor(Math.random() * 10) + 100;
+  let sembol = await db.fetch(`psembol_${message.guild.id}`) || "₺";
 
   let lastDaily = await db.fetch(
     `lastDaily_${message.guild.id}_${message.author.id}`
@@ -23,7 +25,7 @@ exports.run = async (bot, message, args) => {
   } else {
     const embed = new Discord.RichEmbed()
       .setTitle("Çalıştın!")
-      .setDescription(`İşten: **${amount}** ₺ kadar kazandın!`)
+      .setDescription(`${cevaplar}\n**${amount}**${sembol} kadar kazandın!`)
       .setColor("BLACK");
     message.channel.send(embed);
 
