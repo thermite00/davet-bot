2c323ab3d852edb4e930b7cb739bbb37b48d1664
Glitch (davet-yoneticisi) <none>
2019-12-28 13:53:49 +0000

    🚲📰 Checkpoint ./komutlar/davet.js:3324753/76

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message) => {
   
   const embed = new Discord.RichEmbed()
     .setColor("BLACK")
-    .addField(`Botu Ekleyin!`, `http://bit.ly/35kPoCr`)
+    .addField(`Botu Ekleyin!`, `http://bit.ly/davetyöneticisiekle`)
 .setFooter(client.user.username, client.user.avatarURL)
 
   message.channel.send(embed);
