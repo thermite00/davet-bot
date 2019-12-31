3696e932fe1e59079fe73629676fba2a3eb6fa94
Glitch (davet-yoneticisi) <none>
2019-12-31 20:41:03 +0000

    🏠🍇 Checkpoint ./server.js:3324753/5852 ./komutlar/davetlerim.js:3324753/3527 ./komutlar/rütbe-liste.js:3324753/1561 ./komutlar/rütbe-sil.js:3324753/2241 ./komutlar/rütbe-ekle.js:3324753/3062 ./komutlar/yardım.js:3324753/79

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -7,7 +7,7 @@ exports.run = async (bot, message, args, tools) => {
   const embed = new Discord.RichEmbed()
       .setDescription(`Bot sürümü; **v0.2**, Prefix: **${prefix}**, Dil: **tr**`)
   .addField(`Davetler`, `\`davet-kanal\`, \`davet-kanal-sıfırla\`, \`davet-ekle\`, \`davet-sıfırla\`, \`davet-sil\`, \`davet-stokla\`, \`davetlerim\`, \`davet-oluştur\``)
-  .addField(`Rütbeler`, `\`ÇOK YAKINDA! (v0.2)\``)
+  .addField(`Rütbeler`, `\`rütbe-ekle\`, \`rütbe-sil\`, \`rütbe-liste\``)
   .addField(`Bot`, `\`bot-bilgi\`, \`yapımcılar\`, \`davet\`, \`yardım\`, \`üye-durum\`, \`ping\`, \`prefix\`, \`prefix-sıfırla\`, \`destek-sunucu\``)
   .addField(`Premium`, `\`pre-günlük\`, \`pre-puan\`, \`pre-market\``)
   .addField(`Moderasyon`, `\`ban\`, \`kick\`, \`kanal-kilit\`, \`unban\`, \`mute\`, \`takma-ad\`, \`rol-al\`, \`rol-ver\`, \`reklam-taraması\`, \`slowmode\`, \`temizle\``)
