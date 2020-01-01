f66b0aa815aa93346ff1fcdaee9703dd1d2ce25e
Glitch (davet-yoneticisi) <none>
2020-01-01 13:40:44 +0000

    🚠🕶 Checkpoint ./komutlar/rütbe-ekle.js:3324753/6556 ./komutlar/rütbe-liste.js:3324753/3251

--- komutlar/rütbe-ekle.js
+++ komutlar/rütbe-ekle.js
@@ -5,6 +5,7 @@ exports.run = async (client, message, args) => {
   let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "!";
   let veri = await db.fetch(`rol1_${message.guild.id}`);
   let veri2 = await db.fetch(`rol2_${message.guild.id}`);
+  let veri12 = await db.fetch(`roldavet1_${message.guild.id}`)
   if (veri2) {
     const embed = new Discord.RichEmbed()
       .setDescription(`Zaten maksimum rütbe sınırına ulaşmışsınız!`)
@@ -85,10 +86,20 @@ exports.run = async (client, message, args) => {
       .setFooter(client.user.username, client.user.avatarURL);
 
     message.channel.send(embed);
-
+if(enis < veri12){
+  let hhh = await db.fetch(`rol1_${message.guild.id}`)
+  let sss = await db.fetch(`roldavet1_${message.guild.id}`)
+  await db.set(`rol1_${message.guild.id}`, sine.id);
+    await db.set(`roldavet1_${message.guild.id}`, enis);
+await db.set(`rol2_${message.guild.id}`, hhh);
+    await db.set(`roldavet2_${message.guild.id}`, sss);
+  
+    return
+}else{
     await db.set(`rol2_${message.guild.id}`, sine.id);
     await db.set(`roldavet2_${message.guild.id}`, enis);
     return;
+}
   }
 };
 exports.conf = {
--- komutlar/rütbe-liste.js
+++ komutlar/rütbe-liste.js
@@ -5,39 +5,46 @@ exports.run = async (client, message, args) => {
   let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "!";
   let veri = await db.fetch(`rol1_${message.guild.id}`);
   let veri12 = await db.fetch(`roldavet1_${message.guild.id}`);
-    let veri21 = await db.fetch(`roldavet2_${message.guild.id}`);
+  let veri21 = await db.fetch(`roldavet2_${message.guild.id}`);
   let veri2 = await db.fetch(`rol2_${message.guild.id}`);
 
   if (!veri) {
-
     const embed = new Discord.RichEmbed()
-      .setDescription(
-        `Zaten herhangi bir rütbe ayarlanmamış!`
-      )
+      .setDescription(`Zaten herhangi bir rütbe ayarlanmamış!`)
       .setColor("BLACK")
       .setFooter(client.user.username, client.user.avatarURL);
 
     message.channel.send(embed);
   }
   ///////////////////
-  if(veri && !veri2){
-    const embed = new Discord.RichEmbed()
-      .setDescription(
-        `1 - Rol: **${message.guild.roles.get(veri).name}** - ${veri12} Davet!`
-      )
-      .setColor("BLACK")
-      .setFooter(client.user.username, client.user.avatarURL);
+  if (veri) {
+    if (!veri2) {
+      const embed = new Discord.RichEmbed()
+        .setDescription(
+          `1 - Rol: **${
+            message.guild.roles.get(veri).name
+          }** - ${veri12} Davet!`
+        )
+        .setColor("BLACK")
+        .setFooter(client.user.username, client.user.avatarURL);
 
-    message.channel.send(embed);
-  }else{
-    const embed = new Discord.RichEmbed()
-      .setDescription(
-        `1 - Rol: **${message.guild.roles.get(veri).name}** - ${veri12} Davet!\n2 - Rol: **${message.guild.roles.get(veri2).name}** - ${veri21} Davet!`
-      )
-      .setColor("BLACK")
-      .setFooter(client.user.username, client.user.avatarURL);
+      message.channel.send(embed);
+      return;
+    } else {
+      const embed = new Discord.RichEmbed()
+        .setDescription(
+          `1 - Rol: **${
+            message.guild.roles.get(veri).name
+          }** - ${veri12} Davet!\n2 - Rol: **${
+            message.guild.roles.get(veri2).name
+          }** - ${veri21} Davet!`
+        )
+        .setColor("BLACK")
+        .setFooter(client.user.username, client.user.avatarURL);
 
-    message.channel.send(embed);
+      message.channel.send(embed);
+      return;
+    }
   }
 };
 exports.conf = {
